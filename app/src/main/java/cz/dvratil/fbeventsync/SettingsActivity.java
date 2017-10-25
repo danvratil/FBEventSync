9a49c43a9ce0774bb7d2fa1fbb60b95539c16547
Daniel Vrátil <dvratil@kde.org>
2017-10-25 16:09:09 +0200

    Add configuration

---
+++ app/src/main/java/cz/dvratil/fbeventsync/SettingsActivity.java
@@ -0,0 +1,81 @@
+/*
+    Copyright (C) 2017  Daniel Vrátil <me@dvratil.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+package cz.dvratil.fbeventsync;
+
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceFragment;
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    private boolean mShouldForceSync = false;
+
+    private static String CONFIGURE_REMINDERS_ACTION = "cz.dvratil.fbeventsync.Settings.CONFIGURE_REMINDERS";
+    private static String CONFIGURE_SYNC_ACTION = "cz.dvratil.fbeventsync.Settings.CONFIGURE_SYNC";
+    private static String CONFIGURE_MISC_ACTION = "cz.dvratil.fbeventsync.Settings.CONFIGURE_MISC";
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PreferenceFragment fragment = null;
+        String action = getIntent().getAction();
+        if (action == CONFIGURE_REMINDERS_ACTION) {
+            fragment = new ReminderPreferenceFragment();
+        } else if (action == CONFIGURE_SYNC_ACTION) {
+            fragment = new SyncPreferenceFragment();
+        } else if (action == CONFIGURE_MISC_ACTION) {
+            fragment = new MiscPreferenceFragment();
+        }
+        getFragmentManager().beginTransaction().replace(android.R.id.content, fragment).commit();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (mShouldForceSync) {
+            // TODO: Trigger resync
+        }
+    }
+
+    public static class ReminderPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(final Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.reminder_prefrences);
+        }
+    }
+
+    public static class SyncPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.sync_preferences);
+        }
+    }
+
+    public static class MiscPreferenceFragment extends PreferenceFragment {
+        @Override
+        public void onCreate(@Nullable Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.misc_preferences);
+        }
+    }
+}
