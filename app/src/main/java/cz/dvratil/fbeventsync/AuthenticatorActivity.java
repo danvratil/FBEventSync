danvratil/FBEventSync

commit e92ac2da81266b73063997779a4660b37b6cea14

Better logging when we fail to find iCal URI

app/src/main/java/cz/dvratil/fbeventsync/AuthenticatorActivity.java
@@ -74,7 +74,8 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
     protected void onBirthdayLinkExtracted(String s) {
         mLogger.debug("AUTH","Found bday URL");
 
-        if (s.isEmpty()) {
+        if (!s.startsWith("webcal")) {
+            mLogger.debug("AUTH", "Failed to find iCal, debug: %s", s);
             Toast.makeText(this, "Authentication error: failed to retrieve birthday calendar", Toast.LENGTH_LONG)
                     .show();
             finish();
@@ -160,7 +161,7 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
                                         "        return;" +
                                         "    }" +
                                         "  }" +
-                                        "  android.linkExtracted(\"\");" +
+                                        "  android.linkExtracted(document.getElementById(\"events_dashboard_export\").outerHTML);" +
                                         "})();");
                     } else {
                         view.evaluateJavascript(
@@ -172,6 +173,7 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
                                         "      return link.href;" +
                                         "    }" +
                                         "  }" +
+                                        "  return document.getElementById(\"events_dashboard_export\").outerHTML;" +
                                         "})();",
                                 new ValueCallback<String>() {
                                     @Override

commit 4468754395d982fd14b3dcf973de393adcd6990a

Fix authentication
The detection got broken in e92ac2da8 because I'm stupid.

app/src/main/java/cz/dvratil/fbeventsync/AuthenticatorActivity.java
@@ -74,7 +74,7 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
     protected void onBirthdayLinkExtracted(String s) {
         mLogger.debug("AUTH","Found bday URL");
 
-        if (!s.startsWith("webcal")) {
+        if (!s.startsWith("\"webcal")) {
             mLogger.debug("AUTH", "Failed to find iCal, debug: %s", s);
             Toast.makeText(this, "Authentication error: failed to retrieve birthday calendar", Toast.LENGTH_LONG)
                     .show();
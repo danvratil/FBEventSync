4468754395d982fd14b3dcf973de393adcd6990a
Daniel Vrátil <dvratil@kde.org>
2018-02-14 01:00:12 +0100

    Fix authentication
    The detection got broken in e92ac2da8 because I'm stupid.

--- app/src/main/java/cz/dvratil/fbeventsync/AuthenticatorActivity.java
+++ app/src/main/java/cz/dvratil/fbeventsync/AuthenticatorActivity.java
@@ -74,7 +74,7 @@ public class AuthenticatorActivity extends AccountAuthenticatorActivity {
     protected void onBirthdayLinkExtracted(String s) {
         mLogger.debug("AUTH","Found bday URL");
 
-        if (!s.startsWith("webcal")) {
+        if (!s.startsWith("\"webcal")) {
             mLogger.debug("AUTH", "Failed to find iCal, debug: %s", s);
             Toast.makeText(this, "Authentication error: failed to retrieve birthday calendar", Toast.LENGTH_LONG)
                     .show();
